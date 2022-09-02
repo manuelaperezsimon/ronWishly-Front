2895af168a5274a8365f954d4194cd56fd16ab48
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-02 16:31:35 +0200

    Changed tests: register form and page, login form and page

--- src/pages/LoginPage/LoginPageStyled.ts
+++ src/pages/LoginPage/LoginPageStyled.ts
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const LinkStyled = styled.div`
   .login-text__text {
-    margin-top: 25px;
+    margin-top: 0;
     font-size: 15px;
     justify-content: center;
     display: flex;
     gap: 8px;
+    margin-bottom: 25px;
   }
 `;
 
