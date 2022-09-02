17e247eff64860acd0133b1fa6b4b27f187994a1
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-02 09:11:49 +0200

    Created the Login component html, the style and tested it

---
+++ src/components/Login/LoginForm.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import LoginForm from "./LoginForm";
+
+describe("Given a Login Component", () => {
+  describe("When instantiated", () => {
+    test("Then it should display a form with a title, two inputs, a button and a link", () => {
+      render(<LoginForm />);
+
+      const elementsInScreen = [
+        screen.getByAltText("two circles yellows"),
+        screen.getByAltText("ronWishly logo"),
+        screen.getByText("Welcome back!"),
+        screen.getByPlaceholderText("Enter your user name :)"),
+        screen.getByPlaceholderText("Here your password"),
+        screen.getByRole("button"),
+        screen.getByText("Don't have an account?"),
+      ];
+
+      elementsInScreen.forEach((element) => {
+        expect(element).toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ src/components/Login/LoginFormStyled.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const LoginFormStyled = styled.div`
+  display: grid;
+  justify-items: center;
+  background-color: ${(props) => props.theme.greyColor};
+  font-size: 20px;
+
+  h2 {
+    margin: 0px;
+    margin-top: 80px;
+  }
+
+  .circles__picture {
+    position: absolute;
+    top: -75px;
+    left: -50px;
+  }
+
+  .logo__picture {
+    width: 130px;
+    margin-top: 100px;
+    position: relative;
+  }
+
+  .form-login {
+    flex-direction: column;
+    padding: 0;
+    margin: 0 auto;
+    gap: 1rem;
+    width: fit-content;
+    padding: 3rem;
+  }
+
+  .form-login__group {
+    flex-direction: column;
+    display: flex;
+    width: 300px;
+  }
+
+  .form__label {
+    margin-bottom: 10px;
+    margin-left: 17px;
+  }
+
+  .form__input {
+    height: 60px;
+    border-radius: 30px;
+    margin-bottom: 45px;
+    padding: 15px;
+    border: 1rem;
+    font-size: 16px;
+    font-family: inherit;
+  }
+
+  .form__input--wrong {
+    border: solid red 1px;
+  }
+
+  .form-login__link {
+    margin-top: 25px;
+    font-size: 17px;
+    justify-content: center;
+    display: flex;
+    gap: 8px;
+  }
+`;
+
+export { LoginFormStyled };
