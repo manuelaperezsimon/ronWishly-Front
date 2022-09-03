80b111b5968e27cc716c41a4b044acf8af71c120
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-03 16:28:15 +0200

    Created the wishes slice with loadWishesActionCreator and tested it

---
+++ src/store/features/wishes/slices/wishesSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Wishes } from "../../../interfaces/wishesInterfaces";
+
+const wishesInitialState: Wishes = [];
+
+const wishesSlice = createSlice({
+  name: "wishes",
+  initialState: wishesInitialState,
+  reducers: {
+    loadAllWishes: (previousWishes, action: PayloadAction<Wishes>) =>
+      action.payload,
+  },
+});
+
+export const { reducer: wishesReducer } = wishesSlice;
+
+export const { loadAllWishes: loadAllWishesActionCreator } =
+  wishesSlice.actions;
+
+export default wishesSlice.reducer;
---
+++ src/store/interfaces/wishesInterfaces.ts
@@ -0,0 +1,8 @@
+export interface IWish {
+  title: string;
+  picture: string;
+  limitDate: Date;
+  description: string;
+}
+
+export type Wishes = IWish[];
