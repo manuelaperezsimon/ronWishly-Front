950038ec0feb17a76d0de22c886e72e9e39dcb84
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-04 19:10:45 +0200

    Modified the imports in Register test

--- src/pages/RegisterPage/RegisterFormPage.test.tsx
+++ src/pages/RegisterPage/RegisterFormPage.test.tsx
@@ -1,7 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "../../store/store";
 import Wrapper from "../../utils/Wrapper";
 import RegisterFormPage from "./RegisterFormPage";
 
