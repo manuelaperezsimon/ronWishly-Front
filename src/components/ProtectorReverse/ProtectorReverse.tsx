09d60c7f2ece4096556364ee03a20d4bca078978
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-07 18:37:25 +0200

    Created the ProtectorReverse and tested it

--- src/components/ProtectorReverse/ProtectorReverse.tsx
+++ src/components/ProtectorReverse/ProtectorReverse.tsx
@@ -2,21 +2,22 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
 
-interface ProtectorProps {
+interface ProtectorReverseProps {
   children: JSX.Element;
 }
 
-const Protector = ({ children }: ProtectorProps): JSX.Element => {
+const ProtectorReverse = ({ children }: ProtectorReverseProps) => {
   const token = useAppSelector((state) => state.users.token);
   const navigate = useNavigate();
-  const hasToken = token === "" ? false : true;
 
+  const hasToken = token === "" ? false : true;
   useEffect(() => {
-    if (!hasToken) {
-      navigate("/login");
+    if (hasToken) {
+      navigate("/wishes");
     }
   }, [hasToken, navigate]);
 
-  return hasToken ? children : <></>;
+  return !hasToken ? children : <></>;
 };
-export default Protector;
+
+export default ProtectorReverse;
