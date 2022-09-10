57ada065bd741cc40aebfa43af91ea35c910c662
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-10 19:03:00 +0200

    Created the wishFormPage and tested the part of create

---
+++ src/pages/WishFormPage/WishFormPage.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../store/store";
+import WishFormPage from "./WishFormPage";
+
+const mockUseParams = jest.fn();
+const mockUseAppSelector = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => mockUseParams(),
+}));
+
+jest.mock("../../store/hooks", () => ({
+  ...jest.requireActual("../../store/hooks"),
+  useAppSelector: () => mockUseAppSelector(),
+}));
+
+describe("Given a WishFormPage component", () => {
+  describe("When not receives an id", () => {
+    test("Then it should rendered the create component", () => {
+      mockUseParams.mockReturnValue({ id: undefined });
+      mockUseAppSelector.mockReturnValue([]);
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <WishFormPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const expectedTextButton = "Create wish";
+      const button = screen.getByRole("button", {
+        name: expectedTextButton,
+      });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
