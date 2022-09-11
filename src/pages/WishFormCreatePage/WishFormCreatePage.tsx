97259296159b5c55a8994e6b4ad66c0fcba8255b
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-11 16:36:06 +0200

    Deleted the unnecesary imports

--- src/pages/WishFormCreatePage/WishFormCreatePage.tsx
+++ src/pages/WishFormCreatePage/WishFormCreatePage.tsx
@@ -1,11 +1,4 @@
-import { useParams } from "react-router-dom";
 import WishFormCreate from "../../components/WishFormCreate/WishFormCreate";
-import WishForm from "../../components/WishFormCreate/WishFormCreate";
-import { useAppSelector } from "../../store/hooks";
-import {
-  IdWish,
-  NewOrModifyWish,
-} from "../../store/interfaces/wishesInterfaces";
 
 const WishFormPage = (): JSX.Element => {
   return <WishFormCreate />;
